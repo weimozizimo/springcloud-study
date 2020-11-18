9301317c3bdb1adb2e884ea73e7395bfca4df945
wyf <zimo@example.com>
2020-11-18 17:15:05 +0800

    zull提交

--- spring-cloud-consul-zuul/src/main/java/com/wyf/spring/cloud/zuul/filter/MyZuulFilter.java
+++ spring-cloud-consul-zuul/src/main/java/com/wyf/spring/cloud/zuul/filter/MyZuulFilter.java
@@ -1,8 +1,16 @@
 package com.wyf.spring.cloud.zuul.filter;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Log4j2
+@Component
 public class MyZuulFilter extends ZuulFilter {
     @Override
     public String filterType() {
@@ -11,7 +19,7 @@ public class MyZuulFilter extends ZuulFilter {
 
     @Override
     public int filterOrder() {
-        return 5; //定义filter的顺序，数字越小说明优先级越高
+        return 0; //定义filter的顺序，数字越小说明优先级越高
     }
 
     @Override
@@ -21,6 +29,21 @@ public class MyZuulFilter extends ZuulFilter {
 
     @Override
     public Object run() throws ZuulException {
-        return null;
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String token = request.getParameter("token");
+        log.info(token);
+        if(token==null){
+            try {
+                log.warn("there is no request token");
+                ctx.setSendZuulResponse(false);
+                ctx.setResponseStatusCode(401);
+                ctx.getResponse().getWriter().write("this request not have token");
+                return  null;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return  null;
     }
 }
